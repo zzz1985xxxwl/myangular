24d071d870f8af9160d332169ecbf9412f4d3bb5
zzz1985xxxwl <betterman212002@gmail.com>
2015-02-06 08:21:36 +0800

    add

---
+++ app/scripts/controller/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $socpe.now=new Date();
+  });
