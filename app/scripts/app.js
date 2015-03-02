9021d8bdfc5f3ab822282b9c285c3612ec1c99c7
zzz1985xxxwl <betterman212002@gmail.com>
2015-03-02 16:21:06 +0800

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,47 @@
 'use strict';
-define(['angularAMD',
-  'angular-route',
-  'angular-animate',
-  'angular-cookies',
-  'angularLocalStorage',
-  'blockUI',
-  'inform',
-  'plugins/grid',
-  'controllers/sidebar_ctrl',
-  'controllers/login_ctrl',
-  'directives/portlet',
-  'directives/plugins',
-  'directives/validation',
-  'animation/slideDown',
-  'angular-resource',
-  'angular-bootstrap',
-  'angular-bootstrap-show-errors'], function (angularAMD) {
-  var app = angular.module('dl-app', ['ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'ui.bootstrap', 'ui.bootstrap.showErrors','angularLocalStorage','blockUI','inform']);
+define([
+	'angularAMD',
+	'angular-route',
+	'angular-animate',
+	'angular-cookies',
+	'angularLocalStorage',
+	'blockUI',
+	'inform',
+	'jquery',
+	'plugins/grid',
+	'controllers/sidebar_ctrl',
+	'controllers/login_ctrl',
+	'directives/portlet',
+	'directives/plugins',
+	'directives/validation',
+	'animation/slideDown',
+	'angular-resource',
+	'angular-bootstrap',
+	'angular-bootstrap-show-errors'], function (angularAMD) {
+	var app = angular.module('dl-app', ['ngRoute', 'ngAnimate', 'ngCookies', 'ngResource', 'ui.bootstrap', 'ui.bootstrap.showErrors', 'angularLocalStorage', 'blockUI', 'inform']);
 
-  app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/home', angularAMD.route({
-        templateUrl: 'views/home.html', controllerUrl: 'controllers/home_ctrl', nav: 'home'
-      })).when('/table', angularAMD.route({
-        templateUrl: 'views/table.html', controllerUrl: 'controllers/table_ctrl', nav: 'table'
-      }))
-      .otherwise({redirectTo: '/home'});
-  }).config(function(blockUIConfig){
-    blockUIConfig.message='';
-    blockUIConfig.template ='<div class="block-ui-overlay"></div><div class="block-ui-message-container" aria-live="assertive" aria-atomic="true"><div class="loading-message loading-message-boxed"> <div class="block-spinner-bar"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>{{state.message}}</div> </div></div>';
-  }).config(function(informProvider) {
-    informProvider.defaults({
-      ttl: 2000,
-      type: 'danger'
-    });
-  });
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when('/home', angularAMD.route({
+				templateUrl: 'views/home.html', controllerUrl: 'controllers/home_ctrl', nav: 'home'
+			})).when('/table', angularAMD.route({
+				templateUrl: 'views/table.html', controllerUrl: 'controllers/table_ctrl', nav: 'table'
+			}))
+			.otherwise({redirectTo: '/home'});
+	}).config(function (blockUIConfig) {
+		blockUIConfig.message = '';
+		blockUIConfig.template = '<div class="block-ui-overlay"></div><div class="block-ui-message-container" aria-live="assertive" aria-atomic="true"><div class="loading-message loading-message-boxed"> <div class="block-spinner-bar"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div>{{state.message}}</div> </div></div>';
+	}).config(function (informProvider) {
+		informProvider.defaults({
+			ttl: 2000,
+			type: 'danger'
+		});
+	});
 
-  // Define constant
-  //app.constant('SiteName', '/angularAMD');
+	// Define constant
+	//app.constant('SiteName', '/angularAMD');
 
-  // Bootstrap Angular when DOM is ready
-  return angularAMD.bootstrap(app);
+	// Bootstrap Angular when DOM is ready
+	return angularAMD.bootstrap(app);
 
 });
--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,41 +1,44 @@
 'use strict';
 require.config({
-  // alias libraries paths
-  paths: {
-    'jquery': '../../bower_components/jquery/dist/jquery',
-    'angular': '../../bower_components/angular/angular',
-    'angular-route': '../../bower_components/angular-route/angular-route',
-    'angular-animate': '../../bower_components/angular-animate/angular-animate',
-    'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
-    'angular-resource': '../../bower_components/angular-resource/angular-resource',
-    'angular-sanitize': '../../bower_components/angular-sanitize/angular-sanitize',
-    'angular-bootstrap': '../../bower_components/angular-bootstrap/ui-bootstrap-tpls',
-    'angularAMD': '../../bower_components/angularAMD/angularAMD',
-    'angular-bootstrap-show-errors':'../../bower_components/angular-bootstrap-show-errors/src/showErrors',
-    'angularLocalStorage':'../../bower_components/angularLocalStorage/src/angularLocalStorage',
-    'blockUI':'../../bower_components/angular-block-ui/dist/angular-block-ui',
-    'inform':'../../bower_components/angular-inform/dist/angular-inform',
-    'util':'plugins/util'
-  },
+	// alias libraries paths
+	paths: {
+		'jquery': '../../bower_components/jquery/dist/jquery',
+		'angular': '../../bower_components/angular/angular',
+		'angular-route': '../../bower_components/angular-route/angular-route',
+		'angular-animate': '../../bower_components/angular-animate/angular-animate',
+		'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
+		'angular-resource': '../../bower_components/angular-resource/angular-resource',
+		'angular-sanitize': '../../bower_components/angular-sanitize/angular-sanitize',
+		'angular-bootstrap': '../../bower_components/angular-bootstrap/ui-bootstrap-tpls',
+		'angularAMD': '../../bower_components/angularAMD/angularAMD',
+		'angular-bootstrap-show-errors': '../../bower_components/angular-bootstrap-show-errors/src/showErrors',
+		'angularLocalStorage': '../../bower_components/angularLocalStorage/src/angularLocalStorage',
+		'blockUI': '../../bower_components/angular-block-ui/dist/angular-block-ui',
+		'inform': '../../bower_components/angular-inform/dist/angular-inform',
+		'util': 'plugins/util',
+		'drag-event': 'plugins/jquery.event.drag-2.2',
+		'drop-event': 'plugins/jquery.event.drop-2.2'
+	},
 
-  // Add angular modules that does not support AMD out of the box, put it in a shim
-  shim: {
-    'angular':['jquery'],
-    'angularAMD': ['angular'],
-    'angular-route': ['angular'],
-    'angular-animate':['angular'],
-    'angular-cookies':['angular'],
-    'angular-resource':['angular'],
-    'angular-sanitize':['angular'],
-    'angular-bootstrap':['angular'],
-    'angular-bootstrap-show-errors':['angular'],
-    'util':['jquery'],
-    'angularLocalStorage':['angular','angular-cookies'],
-    'blockUI':['angular'],
-    'inform':['angular']
-  },
+	// Add angular modules that does not support AMD out of the box, put it in a shim
+	shim: {
+		'angularAMD': ['angular'],
+		'angular-route': ['angular'],
+		'angular-animate': ['angular'],
+		'angular-cookies': ['angular'],
+		'angular-resource': ['angular'],
+		'angular-sanitize': ['angular'],
+		'angular-bootstrap': ['angular'],
+		'angular-bootstrap-show-errors': ['angular'],
+		'util': ['jquery'],
+		'angularLocalStorage': ['angular', 'angular-cookies'],
+		'blockUI': ['angular'],
+		'inform': ['angular'],
+		'drag-event':['jquery'],
+		'drop-event':['jquery']
+	},
 
-  // kick start application
-  deps: ['app']
+	// kick start application
+	deps: ['app']
 });
 
